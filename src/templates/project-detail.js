7928799e3872d40545bf66d8140d69c5b2be0581
Ankit Saikia <112230331+Saikiasan@users.noreply.github.com>
2024-12-13 14:03:25 +0530

    changes

--- src/templates/project-detail.js
+++ src/templates/project-detail.js
@@ -3,29 +3,58 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import * as s from "../styles/project-home.module.css"
 import "../styles/project-blog.css"
+import { useMediaQuery } from "react-responsive"
 
+const Pc = ({ markdownRemark }) => {
+  return (
+    <div className="container-fluid" style={{ margin: "4rem 0px" }}>
+      <h1 className={s.projectHeading} style={{ fontSize: "4rem" }}>
+        {markdownRemark.frontmatter.title}
+      </h1>
+      <h2 className={s.pStack}>{markdownRemark.frontmatter.stack}</h2>
+      <div
+        className={`card ${s.projectCardBody}`}
+        style={{ marginTop: "3.5rem" }}
+      >
+        <div
+          className={s.pExcerpt}
+          style={{
+            color: "var(--o-90)",
+          }}
+          dangerouslySetInnerHTML={{ __html: markdownRemark.html }}
+        />
+      </div>
+    </div>
+  )
+}
+
+const Mobile = ({ markdownRemark }) => {
+  return (
+    <div className="container-fluid" style={{ margin: "12px 0px" }}>
+      <h1 className={s.mprojectHeading}>{markdownRemark.frontmatter.title}</h1>
+      <h2 className={s.mpStack}>{markdownRemark.frontmatter.stack}</h2>
+      <div className={`${s.mprojectCardBody}`} style={{ marginTop: "24px" }}>
+        <div
+          className={s.mpExcerpt}
+          style={{
+            color: "var(--o-90)",
+          }}
+          dangerouslySetInnerHTML={{ __html: markdownRemark.html }}
+        />
+      </div>
+    </div>
+  )
+}
 const ProjectDetail = ({ data }) => {
+  const isMobile = useMediaQuery({ maxWidth: 768 })
   const { markdownRemark } = data
   return (
     <Layout>
-      <div className="container-fluid" style={{ margin: "4rem 0px" }}>
-        <h1 className={s.projectHeading} style={{ fontSize: "4rem" }}>
-          {markdownRemark.frontmatter.title}
-        </h1>
-        <h2 className={s.pStack}>{markdownRemark.frontmatter.stack}</h2>
-        <div
-          className={`card ${s.projectCardBody}`}
-          style={{ marginTop: "3.5rem" }}
-        >
-          <div
-            className={s.pExcerpt}
-            style={{
-              color: "var(--o-90)",
-            }}
-            dangerouslySetInnerHTML={{ __html: markdownRemark.html }}
-          />
-        </div>
-      </div>
+      {isMobile ? (
+        <Mobile markdownRemark={markdownRemark} />
+      ) : (
+        <Pc markdownRemark={markdownRemark} />
+      )}
     </Layout>
   )
 }
